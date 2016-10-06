b03ec25000f33325c10f2c04f450cf6c88b8e358
amysimmons <amy__simmons@hotmail.com>
2016-10-06 18:07:40 +0100

    sorting by score, still to implement by alphabetical order

---
+++ algorithms/comparator_sort/comparatorSort.js
@@ -0,0 +1,46 @@
+/*
+Given an array of n Player objects, write a comparator that sorts them in order of decreasing score; 
+if 2 or more players have the same score, sort those players alphabetically by name. 
+*/
+
+var players = [
+	{
+		name: 'david',
+		score: 100
+	},
+	{
+		name: 'amy',
+		score: 100
+	},
+	{
+		name: 'heraldo',
+		score: 50
+	},
+	{
+		name: 'aakansha',
+		score: 75
+	},
+	{
+		name: 'aleksa',
+		score: 150
+	},
+
+]
+
+var sorted = false;
+
+while(!sorted){
+	sorted = true;
+
+	for (var i = 0; i < players.length - 1; i++) {
+		if (players[i].score < players[i + 1].score) {
+			var playerToSwap = players[i];
+
+			players[i] = players[i + 1];
+			players[i + 1] = playerToSwap;
+
+			sorted = false;
+		} 
+	}
+	console.log(players);
+}
