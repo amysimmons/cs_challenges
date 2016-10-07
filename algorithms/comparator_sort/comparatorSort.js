9a16c2a808ce0a5e34598e34fdff1fd013dad903
amysimmons <amy__simmons@hotmail.com>
2016-10-07 17:39:35 +0100

    alphabetical sort in progress, some refactoring still to be done

--- algorithms/comparator_sort/comparatorSort.js
+++ algorithms/comparator_sort/comparatorSort.js
@@ -1,6 +1,6 @@
 /*
-Given an array of n Player objects, write a comparator that sorts them in order of decreasing score; 
-if 2 or more players have the same score, sort those players alphabetically by name. 
+Given an array of n Player objects, write a comparator that sorts them in order of decreasing score;
+if 2 or more players have the same score, sort those players alphabetically by name.
 */
 
 var players = [
@@ -29,6 +29,13 @@ var players = [
 
 var sorted = false;
 
+var inAlphabeticalOrder = function(p1, p2) {
+	if (p1.name <= p2.name) {
+		return true
+	}
+	return false;
+}
+
 while(!sorted){
 	sorted = true;
 
@@ -40,7 +47,19 @@ while(!sorted){
 			players[i + 1] = playerToSwap;
 
 			sorted = false;
-		} 
+		}
+		if (players[i].score == players[i + 1].score) {
+			if (!inAlphabeticalOrder(players[i], players[i + 1])) {
+				var playerToSwap = players[i];
+
+				players[i] = players[i + 1];
+				players[i + 1] = playerToSwap;
+
+				sorted = false;
+			}
+
+		}
 	}
 	console.log(players);
 }
+
