699b55423c5dceba02a0887a71844ead053ff5a2
Amy Simmons <amy__simmons@hotmail.com>
2016-10-08 17:39:52 +0100

    extracted swapPlayers into its own function

--- algorithms/comparator_sort/comparatorSort.js
+++ algorithms/comparator_sort/comparatorSort.js
@@ -24,10 +24,30 @@ var players = [
 		name: 'aleksa',
 		score: 150
 	},
-
+	{
+		name: 'tom',
+		score: 200
+	},
+	{
+		name: 'amy',
+		score: 200
+	},
+	{
+		name: 'steve',
+		score: -10
+	}
 ]
 
 var sorted = false;
+
+var swapPlayers = function(i) {
+	var playerToSwap = players[i];
+
+	players[i] = players[i + 1];
+	players[i + 1] = playerToSwap;
+
+	sorted = false;
+}
 
 var inAlphabeticalOrder = function(p1, p2) {
 	if (p1.name <= p2.name) {
@@ -40,26 +60,18 @@ while(!sorted){
 	sorted = true;
 
 	for (var i = 0; i < players.length - 1; i++) {
+		//if the player's score is less than the next one, swap them
 		if (players[i].score < players[i + 1].score) {
-			var playerToSwap = players[i];
-
-			players[i] = players[i + 1];
-			players[i + 1] = playerToSwap;
-
-			sorted = false;
+			swapPlayers(i);
 		}
+		//if the player's score is equal to the next one, alphabetically order them
 		if (players[i].score == players[i + 1].score) {
 			if (!inAlphabeticalOrder(players[i], players[i + 1])) {
-				var playerToSwap = players[i];
-
-				players[i] = players[i + 1];
-				players[i + 1] = playerToSwap;
-
-				sorted = false;
+				swapPlayers(i);
 			}
-
 		}
 	}
-	console.log(players);
 }
+
+console.log(players);
 
