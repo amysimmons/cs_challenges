441095530c8f6b0f85200f795c2de40ddad20385
amysimmons <amy__simmons@hotmail.com>
2016-10-14 19:22:44 +0100

    working on binary search

--- algorithms/binary_search/iceCreamParlor.js
+++ algorithms/binary_search/iceCreamParlor.js
@@ -24,6 +24,7 @@ sample input:
 2 2 4 3 (c cost of each flavor)
 
 sample output:
+
  1 4
  1 2
 */
@@ -43,8 +44,25 @@ var input = {
   ]
 }
 
+/*
+whoaaaaa need a break from this
+*/
+var binarySearch = function(array, n, left, right) {
+
+  var midIndex = Math.floor((left + right) / 2);
+
+  if (array[midIndex] == n) {
+    return midIndex;
+  } else if (n < array[midIndex]) {
+    binarySearch(array, n, left, midIndex - 1);
+  } else {
+    binarySearch(array, n, midIndex + 1, right);
+  }
+}
+
 var whichFlavors = function(input) {
   input.trips.forEach((trip) => {
+
 
   });
 }
@@ -52,3 +70,21 @@ var whichFlavors = function(input) {
 whichFlavors(input);
 
 
+/*
+Binary search is a log n problem because...
+
+We start off with a search space of n elements.
+
+In a single comparison we cut it down to a search space of n/2.
+
+Then in another compairson we cut it down to a search space of n/4.
+
+We continue this until we are left with 1 element.
+
+The total number of operations we will have to do is determined by
+how many times can we diviide n by 2 until we get down to just 1.
+
+This is what a log based 2 event expresses O(log2n).
+
+Binary search can be implemented recursively or iteratively.
+*/
